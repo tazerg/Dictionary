301ac8389570942e9cbbc4032b1285797dd6396e
tazerg <tazerg@yandex.ru>
2024-01-24 18:45:09 +0300

    Окно добавления слов

--- Assets/Scripts/UI/AddWordWindow.cs
+++ Assets/Scripts/UI/AddWordWindow.cs
@@ -1,7 +1,53 @@
+using JHI.Dict.Model;
+using JHI.Dict.Services;
+using UnityEngine.UIElements;
+
 namespace JHI.Dict.UI
 {
     public class AddWordWindow : BaseWindow
     {
+        private TextField _originalTextField;
+        private TextField _translateTextField;
+        private Button _addWordsButton;
+        private Button _closeButton;
+        
+        private IWindowService _windowService;
+        private IAddingWordService _addingWordService;
+        
         public override WindowType WindowType => WindowType.FullScreen;
+
+        private void Start()
+        {
+            _windowService = ServiceLocator.GetService<IWindowService>();
+            _addingWordService = ServiceLocator.GetService<IAddingWordService>();
+
+            var root = GetComponent<UIDocument>().rootVisualElement;
+            _originalTextField = root.Q<TextField>("OriginalText");
+            _translateTextField = root.Q<TextField>("TranslatedText");
+            _addWordsButton = root.Q<Button>("AddButton");
+            _closeButton = root.Q<Button>("CloseButton");
+            
+            _addWordsButton.RegisterCallback<ClickEvent>(OnAddWordButtonClick);
+            _closeButton.RegisterCallback<ClickEvent>(OnCloseClick);
+        }
+        
+        private void OnDestroy()
+        {
+            _addWordsButton.UnregisterCallback<ClickEvent>(OnAddWordButtonClick);
+            _closeButton.RegisterCallback<ClickEvent>(OnCloseClick);
+        }
+
+        private void OnAddWordButtonClick(ClickEvent evt)
+        {
+            var original = _originalTextField.value;
+            var translate = _translateTextField.value;
+            var word = new Word(original, translate);
+            _addingWordService.Add(word);
+        }
+
+        private void OnCloseClick(ClickEvent evt)
+        {
+            _windowService.OpenWindow<MainWindow>();
+        }
     }
 }
